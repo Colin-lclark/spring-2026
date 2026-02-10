5291e88937b72c8c44d7d81f335c9fecbfa80804
Colin-lclark <lc25-0951@lclark.edu>
2026-02-10 15:41:46 -0800

    Update edge.py

--- assignments/pair/p1/edge.py
+++ assignments/pair/p1/edge.py
@@ -1,19 +1,43 @@
 from PIL import Image
 
-def main(imgname):
+if __name__ == '__defineEdge__':
+    defineEdge(Image, list[list(int)])
+
+def defineEdge(imgname, kernal):
     img = Image.open(imgname)
     new_img = img.copy()
-    x, y = 2, 2
+    x, y = 0, 0
     (width, height) = img.size()
-    pixel_coords = [[x-2, y-2],[x,y]]
     while x < width:
         while y < height:
-            setpixels(img, pixel_coords, new_img)
+            pixel_coords = getCoords(x,y)
+            setPixel(img, pixel_coords, new_img, kernal)
             y += 1
         x += 1
+    new_img.show()
 
+def getCoords(x, y):
+    coords = []
+    for x in range(3):
+        coords.append([])
+        for y in range(3):
+            coords[x].append([x,y])
+    return coords
 
-def setPixels(img, pixel_coords, new_img):
+def setPixel(img, pixel_coords, new_img, kernal):
+    pixels = getPixels(img, pixel_coords)
+    new_p = pixels[1][1] * kernal[1][1]
+    for x in range(3):
+        for y in range(3):
+            if x != 1 and y != 1:
+                new_p += pixels[x][y] * kernal[x][y]
+    new_img.putpixel((pixel_coords[1][1][0], pixel_coords[1][1][1]), new_p)
 
 
 def getPixels(img, pixel_coords):
+    pixels = []
+    for x in range(3):
+        pixels.append([])
+        for y in range(3):
+            pixels[x].append(img.getpixel(pixel_coords[x][y]))
+    return pixels 
