cb4f4fef3506facdc9d23c81a0aec4d12a2a22c8
Colin-lclark <lc25-0951@lclark.edu>
2026-02-13 15:09:44 -0800

    Update edge.py

--- assignments/pair/p1/edge.py
+++ assignments/pair/p1/edge.py
@@ -5,11 +5,11 @@ kernal_edge = [[-1,-1,-1],[-1, 8,-1],[-1,-1,-1]]
 def defineEdge(imgname):
     img = Image.open(imgname)
     new_img = img.copy()
-    x = 1
+    x = 0
     (width, height) = img.size
-    while x < width:
-        y = 1
-        while y < height:
+    while x < width - 2:
+        y = 0
+        while y < height - 2:
             pixel_coords = getCoords(x,y)
             setPixel(img, pixel_coords, new_img, kernal_edge)
             y += 1
@@ -28,12 +28,17 @@ def getCoords(x, y):
 
 def setPixel(img, pixel_coords, new_img, kernal):
     pixels = getPixels(img, pixel_coords)
-    new_p = pixels[1][1] * kernal[1][1]
+    new_p = pixels[1][1] * kernal[0][0]
     for x in range(3):
         for y in range(3):
             if x != 1 and y != 1:
                 new_p += pixels[x][y] * kernal[-x][-y]
-    new_img.putpixel((pixel_coords[1][1][0], pixel_coords[1][1][1]), new_p)
+    if new_p < 0:
+        new_p = 0
+    elif new_p > 255:
+        new_p = 255
+    new_p = (new_p, new_p, new_p)
+    new_img.putpixel(((pixel_coords[1][1][0], pixel_coords[1][1][1])), new_p)
 
 
 def getPixels(img, pixel_coords):
@@ -41,8 +46,8 @@ def getPixels(img, pixel_coords):
     for x in range(3):
         pixels.append([])
         for y in range(3):
-            pixels[x].append(img.getpixel(pixel_coords[x][y])[0])
+            pixels[x].append(img.getpixel((pixel_coords[x][y][0], pixel_coords[x][y][1]))[0])
     return pixels 
 
 if __name__ == '__main__':
-    defineEdge(r'C:\Users\Colin\OneDrive\Documents\GitHub\ComputerScienceII\spring-2026\assignments\pair\p1\llama.jpg')
+    defineEdge(r'C:\Users\Colin\OneDrive\Documents\GitHub\ComputerScienceII\spring-2026\assignments\pair\p1\rose.jpg')
