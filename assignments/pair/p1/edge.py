4723c64cc8c0c06ebbe179606254cafdece6997e
Colin-lclark <lc25-0951@lclark.edu>
2026-02-13 15:50:31 -0800

    Update edge.py

--- assignments/pair/p1/edge.py
+++ assignments/pair/p1/edge.py
@@ -1,8 +1,10 @@
 from PIL import Image
 
-kernal_edge = [[-1,-1,-1],[-1, 8,-1],[-1,-1,-1]]
+"""kernal_edge = [[-1,-1,-1],[-1, 8,-1],[-1,-1,-1]]"""
 
-def defineEdge(imgname):
+def defineEdge(imgname, kernal):
+    """Get the input image and kernal, create a new image, and sequentially 
+    set each pixel to a new value based on edge detection."""
     img = Image.open(imgname)
     new_img = img.copy()
     x = 0
@@ -10,44 +12,36 @@ def defineEdge(imgname):
     while x < width - 2:
         y = 0
         while y < height - 2:
-            pixel_coords = getCoords(x,y)
-            setPixel(img, pixel_coords, new_img, kernal_edge)
+            pixel_coords = (x,y)
+            setEdge(img, pixel_coords, new_img, kernal)
             y += 1
         x += 1
     new_img.show()
 
-def getCoords(x, y):
-    coords = []
-    for x1 in range(3):
-        coords.append([])
-        x1 += 1
-        for y1 in range(3):
-            coords[x1-1].append([x+x1-1,y+y1-1])
-            y1 += 1
-    return coords
-
-def setPixel(img, pixel_coords, new_img, kernal):
-    pixels = getPixels(img, pixel_coords)
-    new_p = pixels[1][1] * kernal[0][0]
+def setEdge(img, pixel_coords, new_img, kernal):
+    """Get list of three by three pixels and set the center pixel to a new value based on the kernal."""
+    pixels = getPixelList(img, pixel_coords)
+    new_p = pixels[1][1] * kernal[1][1]
     for x in range(3):
         for y in range(3):
-            if x != 1 and y != 1:
-                new_p += pixels[x][y] * kernal[-x][-y]
+            if (x,y) != (1,1):
+                new_p += pixels[x][y] * kernal[x][y]
     if new_p < 0:
         new_p = 0
     elif new_p > 255:
         new_p = 255
     new_p = (new_p, new_p, new_p)
-    new_img.putpixel(((pixel_coords[1][1][0], pixel_coords[1][1][1])), new_p)
+    new_img.putpixel(((pixel_coords[0] + 1, pixel_coords[1] + 1)), new_p)
 
 
-def getPixels(img, pixel_coords):
+def getPixelList(img, pixel_coords):
+    """Get a list of 3x3 pixel coordinates based on the top left pixel coordinate in img."""
     pixels = []
     for x in range(3):
         pixels.append([])
         for y in range(3):
-            pixels[x].append(img.getpixel((pixel_coords[x][y][0], pixel_coords[x][y][1]))[0])
+            pixels[x].append(img.getpixel((pixel_coords[0]+x, pixel_coords[1]+y))[0])
     return pixels 
 
 if __name__ == '__main__':
-    defineEdge(r'C:\Users\Colin\OneDrive\Documents\GitHub\ComputerScienceII\spring-2026\assignments\pair\p1\rose.jpg')
+    defineEdge(filePath = input(), kernal = input())
