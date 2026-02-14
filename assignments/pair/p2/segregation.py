c8066e8b01212952c524cbe90d33a61ec9f35ab9
lukaclazar <lazarlukac2004@gmail.com>
2026-02-13 19:48:26 -0800

    Update segregation.py

--- assignments/pair/p2/segregation.py
+++ assignments/pair/p2/segregation.py
@@ -4,10 +4,13 @@ import random
 GRAPH_SIZE = 50
 WINDOW_SIZE = (500, 500)
 
+def grid(red_perc, blue_perc graph_size, window_size):
+
+    return[]
+
 def segregateSquares(empty_perc, red_perc, blue_perc, segregation_perc, graph_size, window_size):
     return []
 
-def 
 
 def placeSquares(red_perc, blue_perc, empty_perc, graph) -> list[list[str]]:
     return None
