975681250bd1f5048a6b200625bea7294b1a6ce5
Colin-lclark <lc25-0951@lclark.edu>
2026-02-25 17:30:59 -0800

    Outline of Segregation and Tests

---
+++ assignments/pair/p2/segregation.py
@@ -0,0 +1,17 @@
+from graphics import *
+import random
+
+GRAPH_SIZE = 50
+WINDOW_SIZE = (500, 500)
+
+def segregateSquares(empty_perc, red_perc, blue_perc, segregation_perc, graph_size, window_size):
+    return []
+
+def placeSquares(red_perc, blue_perc, empty_perc, graph):
+    return None
+
+def checkGraphSegregation(graph_array) -> int:
+    return 1
+
+def adjustSegregation(graph, segregation_perc):
+    return []
---
+++ assignments/pair/p2/segregation_test.py
@@ -0,0 +1,7 @@
+import segregation
+
+def test_percent_of_colors_in_graph():
+
+
+def test_segregation_check():
+    
