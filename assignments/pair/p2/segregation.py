00b889f736002648d5f4aa9165650322b76e03a9
Colin-lclark <lc25-0951@lclark.edu>
2026-02-26 19:56:44 -0800

    Update segregation.py

--- assignments/pair/p2/segregation.py
+++ assignments/pair/p2/segregation.py
@@ -4,14 +4,14 @@ import random
 GRAPH_SIZE = 50
 WINDOW_SIZE = (500, 500)
 
-def segregateSquares(empty_perc, red_perc, blue_perc, segregation_perc, graph_size, window_size):
+def segregateSquares(empty_perc : int, red_perc : int, blue_perc : int, segregation_perc : int, graph_size : int, window_size : tuple):
     return []
 
 def placeSquares(red_perc : int, blue_perc: int, empty_perc : int) -> list[list[str]]:
     return None
 
-def checkGraphSegregation(graph_array) -> int:
+def checkGraphSegregation(graph_array : list[list[str]]) -> int:
     return 1
 
-def adjustSegregation(graph, segregation_perc):
+def adjustSegregation(graph : list[list[str]], segregation_perc : int) -> list[list[str]]:
     return []
