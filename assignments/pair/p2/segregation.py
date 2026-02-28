23dd036cdd60c55c06dd852f158664e060f8245f
Colin-lclark <lc25-0951@lclark.edu>
2026-02-28 13:33:34 -0800

    Update segregation.py

--- assignments/pair/p2/segregation.py
+++ assignments/pair/p2/segregation.py
@@ -5,7 +5,7 @@ GRAPH_SIZE = 50
 WINDOW_SIZE = (500, 500)
 RED = "red"
 BLUE = "blue"
-EMPTY = "empty"
+EMPTY = None
 
 def grid(RED : str, BLUE : str, EMPTY : str, red_perc : str, blue_perc : str, graph_size : int) -> list[list[str]]:
 
@@ -43,18 +43,25 @@ def grid(RED : str, BLUE : str, EMPTY : str, red_perc : str, blue_perc : str, gr
 def segregateSquares(empty_perc : int, red_perc : int, blue_perc : int, segregation_perc : int, graph_size : int, window_size : tuple):
     return []
 
-def checkSquareSatisfaction(board : list[list[str]], RED : str, BLUE : str, EMPTY : str, graph_size : int) -> int:
+def checkSquareSatisfaction(board : list[list[str]], RED : str, BLUE : str, EMPTY : str, segregation_perc : int, graph_size : int) -> int:
     for row in range(graph_size - 2):
         for column in range(graph_size - 2):
-            if board[row+1][column+1] == RED:
-                check
+            if board[row][column] == RED:
+                isSatisfied(board, row, column, RED, segregation_perc)
             elif board[row+1][column+1] == BLUE:
+                isSatisfied(board, row, column, BLUE, segregation_perc)
 
     return 1
 
-def checkNeighbors(neighborhood : list[list[str]], coords : tuple, color : str) -> str:
-    row =
-    while row 
+def isSatisfied(neighborhood : list[list[str]], row : int, column : int, color : str, segregation_perc : int) -> bool:
+    total = 0
+    matching = 0
+    for row in range(row, row + 2):
+        for column in range(column, column + 2):
+
+
+
+
 
 def adjustSegregation(graph : list[list[str]], segregation_perc : int) -> list[list[str]]:
     return []
